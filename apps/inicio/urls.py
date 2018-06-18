7d747becc1966445b1af031ba12f289fa88a6445
Ricardo Estupinian <ricaeslo@gmail.com>
2018-06-18 09:18:25 -0600

    Se agrego el template informacion, donde se encuentra la informacion basica de uso de la apliacion web, y datos de los desarrolladores

--- apps/inicio/urls.py
+++ apps/inicio/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
-from apps.inicio.views import inicio
+from apps.inicio.views import inicio,infor
 urlpatterns = [
     url(r'^$', inicio,name='inicio'),
+    url(r'^infor$', infor,name='informacion'),
 ]
