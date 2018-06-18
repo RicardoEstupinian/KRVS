1f071bf1cfb317a837a7fdb6c1b6043e7aa8a2fc
Ricardo Estupinian <ricaeslo@gmail.com>
2018-06-17 22:34:33 -0600

    Se agregaron los enlaces al menu principal, se termino esteticamente el template simulacion, se realizaron leves cambios en el template modificacion

--- apps/solicitud/urls.py
+++ apps/solicitud/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
 from apps.solicitud.views import modificacion 
 urlpatterns = [
-    url(r'^$', modificacion),
+    url(r'^$', modificacion,name='modificacion'),
 ]
--- apps/solicitud/views.py
+++ apps/solicitud/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def modificacion(request):
 	return render(request,'modificacion/index_modificacion.html')
+
+
