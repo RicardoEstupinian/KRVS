10dd6b895adf85056c19f8e874e07957d4dbe702
Ricardo Estupinian <ricaeslo@gmail.com>
2018-06-20 19:16:49 -0600

    Se agrego la validacion de dejar vacios los campos, pero hay un problema cuando se da click en simular se deshabilita el boton, se debe de arreglar ese problema

---
+++ static/js/validacion_vacios.js
@@ -0,0 +1,18 @@
+function habilitar(){
+    var c1=document.getElementById("1");
+    var c2=document.getElementById("2");
+    var c3=document.getElementById("3");
+    var c4=document.getElementById("4");
+    var c5=document.getElementById("5");
+    var c6=document.getElementById("6");
+    var c7=document.getElementById("7");
+    var c8=document.getElementById("8");
+
+    var btn=document.getElementById("btnsimular");
+
+    if(c1.value!= "" && c2.value!= "" && c3.value!="" && c4.value!="" && c5.value!="" && c6.value!="" && c7.value!="" && c8.value!=""){
+        btn.disabled=false;
+    }else{
+        btn.disabled=true;
+    }
+}
