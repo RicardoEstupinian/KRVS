e6f6902bd460a29741660a5f854c422cb7481067
kilmer <ml16006@ues.edu.sv>
2018-06-24 18:22:21 -0600

    Se cambiaron los tipos de entrada de algunos campos de la pantalla de simulacion

--- apps/simulacion/models.py
+++ apps/simulacion/models.py
@@ -5,7 +5,7 @@ from apps.log.models import Usuario
 
 # Create your models here.
 class Simulador(models.Model):
-	numero_Cables = models.FloatField(null=False)
+	numero_Cables = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
 	carga_Nominal = models.FloatField(null=False)
 	recorrido_Cabina =  models.FloatField(null=False)
 	recorido_Superior_Piston  = models.FloatField(null=False)
